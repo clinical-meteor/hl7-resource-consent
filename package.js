8ea506b836d66abc7fbc71ca9ce5daa05eef4844
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-10-10 00:46:23 -0500

    post-baltimore sync

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-consent',
-  version: '3.4.11',
+  version: '3.4.12',
   summary: 'HL7 FHIR Resource - Consent',
   git: 'https://github.com/clinical-meteor/hl7-resource-consent',
   documentation: 'README.md'
