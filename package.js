90a9e14a3eb97873a7052ebdb5b1ff40fbab2f91
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-10-24 23:58:39 -0500

    duke thr meeting sync

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-consent',
-  version: '3.4.16',
+  version: '3.4.17',
   summary: 'HL7 FHIR Resource - Consent',
   git: 'https://github.com/clinical-meteor/hl7-resource-consent',
   documentation: 'README.md'
